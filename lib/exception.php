1ab97ab8e79b619647897f80f991fd62fbbb251d
Ric Allinson <capecodehq@yahoo.co.uk>
2013-08-07 15:55:23 -0700

    Update to UUID code

---
+++ lib/exception.php
@@ -0,0 +1,8 @@
+<?php
+namespace php_require\php_uuid;
+
+/**
+ * @package phpcassa\Util
+ */
+class UUIDException extends \Exception {
+}
--- test/uuid.php
+++ test/uuid.php
@@ -7,18 +7,41 @@ $module = new stdClass();
     Now we "require()" the file to test.
 */
 
+$require = function () {};
+
+require(__DIR__ . "/../lib/exception.php");
+require(__DIR__ . "/../lib/clock.php");
 require(__DIR__ . "/../index.php");
+
+$uuid = $module->exports;
 
 /*
     Now we test it.
 */
 
-describe("php-uuid", function () {
+describe("php-uuid", function () use ($uuid){
 
-    describe("uuid->generate()", function () {
-        it("should return", function () {
-            $uuid = new UUID();
-            assert(strlen($uuid->generate(UUID::UUID_TIME, UUID::FMT_STRING)) === 36);
-        });
+    it("should return UUID1 from no arguments", function () use ($uuid) {
+        assert(strlen($uuid()) === 36);
+    });
+
+    it("should return UUID1", function () use ($uuid) {
+        assert(strlen($uuid(1)) === 36);
+    });
+
+    it("should return null", function () use ($uuid) {
+        assert(strlen($uuid(2)) === 0);
+    });
+
+    it("should return UUID3", function () use ($uuid) {
+        assert(strlen($uuid(3, "nodestring", "0787986e-fa45-11e2-b5a0-000000000000")) === 36);
+    });
+
+    it("should return UUID4", function () use ($uuid) {
+        assert(strlen($uuid(4)) === 36);
+    });
+
+    it("should return UUID5", function () use ($uuid) {
+        assert(strlen($uuid(5, "nodestring", "0787986e-fa45-11e2-b5a0-000000000000")) === 36);
     });
 });
